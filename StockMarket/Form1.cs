e73e8aa2f7e32f430c82d74ce6f746cf75bc2f90
Ali <ali62n62@yahoo.com>
2018-12-21 20:02:20 +0330

    Add project files.

---
+++ StockMarket/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using StockMarket.Model;
+
+namespace StockMarket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+       private void ReadData()
+        {
+            using (var reader = new StreamReader(@"C:\Users\test\Documents\TseClient 2.0\ghadir.csv",Encoding.Unicode))
+            {
+                List<StockPointData> listA = new List<StockPointData>();
+                StockPointData tempPoint;
+                
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    tempPoint = new StockPointData();
+                    tempPoint.StockName = values[0];
+                    tempPoint.Date = values[1];
+                    tempPoint.FirstPrice = values[2];
+                    tempPoint.MaxPrice = values[3];
+                    tempPoint.MinPrice = values[4];
+                    tempPoint.FinalPrice = values[5];
+                    tempPoint.Voume = values[6];
+                    tempPoint.Value = values[7];
+                    tempPoint.NumberOfDeals = values[8];
+                    tempPoint.YesterdayPrice = values[9];
+                    tempPoint.CompanyCode = values[10];
+                    tempPoint.LatinName = values[11];
+                    tempPoint.CompanyName = values[12];
+                    tempPoint.Date2 = values[13];
+                    tempPoint.LastPrice = values[14];
+                    listA.Add(tempPoint);                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadData();
+        }
+    }
+}
+
---
+++ StockMarket/Model/StockPointData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Model
+{
+    public class StockPointData
+    {
+        public string StockName { get; set; }
+        public string Date { get; set; }
+        public string FirstPrice { get; set; }
+        public string MaxPrice { get; set; }
+        public string MinPrice { get; set; }
+        public string FinalPrice { get; set; }
+        public string Voume { get; set; }
+        public string Value { get; set; }
+        public string NumberOfDeals { get; set; }
+        public string YesterdayPrice { get; set; }
+        public string CompanyCode { get; set; }
+        public string LatinName { get; set; }
+        public string CompanyName { get; set; }
+        public string Date2 { get; set; }
+        public string LastPrice { get; set; }        
+
+    }
+}
