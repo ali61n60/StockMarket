5b425536fb9558ebceb59a59f9f3a7c9c3b2b4e8
Ali <ali62n62@yahoo.com>
2018-12-22 02:52:37 +0330

    1/10/97

--- StockMarket/Form1.cs
+++ StockMarket/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using StockMarket.Model;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace StockMarket
 {
@@ -20,11 +21,11 @@ namespace StockMarket
         }
 
 
-       private void ReadData()
+       private List<StockPointData> ReadData()
         {
+            List<StockPointData> listA = new List<StockPointData>();
             using (var reader = new StreamReader(@"C:\Users\test\Documents\TseClient 2.0\ghadir.csv",Encoding.Unicode))
-            {
-                List<StockPointData> listA = new List<StockPointData>();
+            {                
                 StockPointData tempPoint;
                 
                 while (!reader.EndOfStream)
@@ -50,11 +51,45 @@ namespace StockMarket
                     listA.Add(tempPoint);                    
                 }
             }
+            return listA;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ReadData();
+           var listData= ReadData();
+            listData.RemoveAt(0);
+            Series price = new Series("price"); // <<== make sure to name the series "price"
+            chart1.Series.Add(price);
+
+            // Set series chart type
+            chart1.Series["price"].ChartType = SeriesChartType.Candlestick;
+
+            // Set the style of the open-close marks
+            chart1.Series["price"]["OpenCloseStyle"] = "Triangle";
+
+            // Show both open and close marks
+            chart1.Series["price"]["ShowOpenClose"] = "Both";
+
+            // Set point width
+            chart1.Series["price"]["PointWidth"] = "1.0";
+
+            // Set colors bars
+            chart1.Series["price"]["PriceUpColor"] = "Green"; // <<== use text indexer for series
+            chart1.Series["price"]["PriceDownColor"] = "Red"; // <<== use text indexer for series
+
+            for (int i = 0; i < listData.Count; i++)
+            {
+                // adding date and high
+                chart1.Series["price"].Points.AddXY(i, double.Parse(listData[i].MaxPrice));
+                // adding low
+                double low = double.Parse(listData[i].MinPrice);
+                chart1.Series["price"].Points[i].YValues[1] = low;
+                //adding open
+                chart1.Series["price"].Points[i].YValues[2] = double.Parse(listData[i].FirstPrice);
+                // adding close
+                chart1.Series["price"].Points[i].YValues[3] = double.Parse(listData[i].LastPrice);
+            }
+
         }
     }
 }
