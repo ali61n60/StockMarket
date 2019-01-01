490481d62c14fe586a1bbfff18288c99624a30ab
Ali <ali62n62@yahoo.com>
2019-01-02 01:01:33 +0330

    working on foundation

---
+++ StockMarketTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using System;
+using System.Globalization;
+
+namespace StockMarketTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+       
+    }
+}
