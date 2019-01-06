86d3b90e91c457bdaca2670668c6546d2b9fcc69
Ali <ali62n62@yahoo.com>
2019-01-06 23:49:15 +0330

    16/10/97

--- StockMarket/Form1.Designer.cs
+++ StockMarket/Form1.Designer.cs
@@ -150,7 +150,7 @@
             // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(519, 12);
