0471449b3278d3e7fa47f384553062298f7282d3
Ali <ali62n62@yahoo.com>
2019-02-02 23:54:35 +0330

    work on stock tarding form

---
+++ StockMarket/FormStockTraing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockMarket
+{
+    public partial class FormStockTraing : Form
+    {
+        public FormStockTraing()
+        {
+            InitializeComponent();
+        }
+    }
+}
