3b84d19d752d7cfb48cfc72233a2182a666b7cd9
Ali <ali62n62@yahoo.com>
2019-02-09 21:38:45 +0330

    accord samples

---
+++ StockMarketTests/ServiceStdTests/AITests/AccordHeoolWorldTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using RepositoryStd.Database;
+using ServiceStd;
+using ServiceStd.AI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockMarketTests.ServiceStdTests.AITests
+{
+    [TestFixture]
+    class AccordHelloWorldTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void XORTest()
+        {
+            AccordHelloWorld accordHelloWorld = new AccordHelloWorld();
+            accordHelloWorld.XOR();
+            Assert.True(true);
+
+        }
+
+        [Test]
+        public void LinearRegressionTest()
+        {
+            AccordHelloWorld accordHelloWorld = new AccordHelloWorld();
+            accordHelloWorld.LinearRegression();
+            Assert.True(true);
+
+        }
+    }
+}
