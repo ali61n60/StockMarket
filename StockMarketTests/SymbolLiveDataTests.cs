91923d0838c393ab7760e6ff409c6d35515d38c0
Ali <ali62n62@yahoo.com>
2020-05-12 14:30:36 +0430

    getting live data

--- StockMarketTests/SymbolLiveDataTests.cs
+++ StockMarketTests/SymbolLiveDataTests.cs
@@ -13,7 +13,8 @@ namespace StockMarketTests
         [Test]
         public void CanDownloadDataFromUrl()
         {
-            SymbolLiveData symbolLiveData = new SymbolLiveData("http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i=26014913469567886&c=39+");
+            //SymbolLiveData symbolLiveData = new SymbolLiveData("http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i=26014913469567886&c=39+");
+            SymbolLiveData symbolLiveData = new SymbolLiveData("http://www.tsetmc.com/tsev2/data/instinfodata.aspx?i=6110133418282108&c=44+");
             Task<SymbolData> symbolDataTask = symbolLiveData.GetLiveDataAsync();
 
             SymbolData symbolData = symbolDataTask.Result;
