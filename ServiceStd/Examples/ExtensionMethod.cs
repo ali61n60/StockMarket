2e712cadbb4d9cfaf999538169a4459767bcba6f
ali <ali@ALI-PC>
2021-03-06 00:11:50 +0330

    15/12/99

---
+++ ServiceStd/Examples/ExtensionMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceStd.Examples
+{
+    public class ShoppingCart
+    {
+        public List<Product> Products;
+    }
+
+    public class Product
+    {
+        public decimal Price;
+    }
+
+
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (Product prod in cartParam.Products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+    }
+}
