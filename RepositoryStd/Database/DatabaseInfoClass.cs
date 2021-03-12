3c80e790166e824bd3fb62aa310448014e445e71
ali <ali@ALI-PC>
2021-03-12 20:26:23 +0330

    22/12/99

--- RepositoryStd/Database/DatabaseInfoClass.cs
+++ RepositoryStd/Database/DatabaseInfoClass.cs
@@ -12,8 +12,8 @@
         public static string DefaultConnectionString()
         {
             //return "Data Source = www.whereismycar.ir\\MSSQLSERVER2012; Initial Catalog = whereism_database; Persist Security Info = True; User ID = whereism_ali; Password = Lam8u38@; MultipleActiveResultSets = true";
-            return "Data Source= .\\;Initial Catalog=StockDb;Persist Security Info=True;User ID=ayoobfar_ali;Password=119801;MultipleActiveResultSets=true";
-            //return "Data Source = www.elecontrol.ir\\MSSQLSERVER2019; Initial Catalog = elecont1_data; Persist Security Info = True; User ID = elecont1_nejati; Password = Ali11980*; MultipleActiveResultSets = true";
+            //return "Data Source= .\\;Initial Catalog=StockDb;Persist Security Info=True;User ID=ayoobfar_ali;Password=119801;MultipleActiveResultSets=true";
+            return "Data Source = www.elecontrol.ir\\MSSQLSERVER2019; Initial Catalog = elecont1_data; Persist Security Info = True; User ID = elecont1_nejati; Password = Ali11980*; MultipleActiveResultSets = true";
         }
     }
 }
