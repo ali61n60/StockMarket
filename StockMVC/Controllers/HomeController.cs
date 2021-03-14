aa3c81d93b440e5f965c4ab879acdca34418cae5
ali <ali@ALI-PC>
2021-03-14 05:08:12 +0330

    moving good enough

--- StockMVC/Controllers/HomeController.cs
+++ StockMVC/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ModelStd.IRepository;
+using ServiceStd.IOC;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +15,11 @@ namespace StockMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            //Get All Symbols data
+            ISymbolInfo symbolInfo= Bootstrapper.container.GetInstance<ISymbolInfo>();
+            List<string> symbolNames= symbolInfo.GetAllSymbolsName();
+
+            return View(symbolNames);
 
             //returning static html files
             //return File("/html/demo.html", "text/html");
