21bba3f592abee4325a82a9efd3c705a7589a489
Ali <ali62n62@yahoo.com>
2021-03-18 03:56:20 +0330

    styling the design

--- RepositoryStd/Database/DatabaseInfoClass.cs
+++ RepositoryStd/Database/DatabaseInfoClass.cs
@@ -11,8 +11,9 @@
 
         public static string DefaultConnectionString()
         {            
-            return "Data Source= .\\;Initial Catalog=StockDb;Persist Security Info=True;User ID=ayoobfar_ali;Password=119801;MultipleActiveResultSets=true";
-            //return "Data Source = www.elecontrol.ir\\MSSQLSERVER2019; Initial Catalog = elecont1_data; Persist Security Info = True; User ID = elecont1_nejati; Password = Ali11980*; MultipleActiveResultSets = true";
+            //return "Data Source= .\\;Initial Catalog=StockDb;Persist Security Info=True;User ID=ayoobfar_ali;Password=119801;MultipleActiveResultSets=true";
+            return "Data Source = www.elecontrol.ir\\MSSQLSERVER2019; Initial Catalog = elecont1_data; Persist Security Info = True; User ID = elecont1_nejati; Password = Ali11980*; MultipleActiveResultSets = true";
+            //plesk3.tegrahost.com,2019
         }
     }
 }
--- StockMVC/Infrastructure/PageLinkTagHelper.cs
+++ StockMVC/Infrastructure/PageLinkTagHelper.cs
@@ -24,6 +24,11 @@ namespace StockMVC.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context,
         TagHelperOutput output)
         {
@@ -34,6 +39,12 @@ namespace StockMVC.Infrastructure
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
                 new { productPage = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
