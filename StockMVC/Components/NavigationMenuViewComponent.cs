54a7f5a50674fb457b8ebceff4bc6a9a2b904e5c
anejati <anejati@115Q216Q>
2021-04-03 15:16:05 +0430

    14/1/1400

---
+++ StockMVC/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ModelStd;
+
+namespace StockMVC.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
