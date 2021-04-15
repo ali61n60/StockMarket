12399bc8918f52b5e7e0d96432dad1503835eed3
Ali <ali62n62@yahoo.com>
2021-04-15 20:33:48 +0430

    adding Newtonsoft.Json

---
+++ ModelStd/Cart.cs
@@ -0,0 +1,42 @@
+﻿using ModelStd.DB.Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelStd
+{
+    public class Cart
+    {
+        public List<SymbolLine> Lines { get; set; } = new List<SymbolLine>();
+        public void AddItem(Symbol symbol, int quantity)
+        {
+            SymbolLine line = Lines
+            .Where(s => s.Symbol.Id == symbol.Id)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                Lines.Add(new SymbolLine
+                {
+                    Symbol = symbol,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(Symbol symbol) =>
+        Lines.RemoveAll(l => l.Symbol.Id == symbol.Id);
+        public decimal ComputeTotalValue() =>
+        Lines.Sum(e => e.Symbol.GroupId * e.Quantity);
+        public void Clear() => Lines.Clear();
+    }
+    public class SymbolLine
+    {
+        public int SymbolLineId { get; set; }
+        public Symbol Symbol { get; set; }
+        public int Quantity { get; set; }
+    }
+}
