0127f9f51b85b1b2325ef37bb582fc5eda3d87b6
Ali <ali62n62@yahoo.com>
2021-05-08 00:53:23 +0430

    order processing

---
+++ StockMVC/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ModelStd.AspBook;
+using ModelStd.DB.Stock;
+using ModelStd.IRepository;
+using ServiceStd.IOC;
+using StockMVC.Models.ViewModels;
+
+namespace StockMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() =>  View(new Order());
+        
+    }
+}
