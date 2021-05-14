e4d5b503785c7fb4f226ba04a4887ef473657571
Ali <ali62n62@yahoo.com>
2021-05-14 21:13:31 +0430

    page 205

---
+++ ModelStd/IRepository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ModelStd.AspBook;
+
+namespace ModelStd.IRepository
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
---
+++ RepositoryStd/Database/Repository/EFOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModelStd.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryStd.Database.Repository
+{
+    class EFOrderRepository : IOrderRepository
+    {
+
+    }
+}
