306ca56092a83e3c5f458f8a55db7fd2df3dc3cb
anejati <anejati@115Q216Q>
2021-05-15 15:18:57 +0430

    vs2019 at work

--- RepositoryStd/Database/Repository/EFOrderRepository.cs
+++ RepositoryStd/Database/Repository/EFOrderRepository.cs
@@ -1,12 +1,32 @@
-﻿using ModelStd.IRepository;
+﻿using Microsoft.EntityFrameworkCore;
+using ModelStd.AspBook;
+using ModelStd.IRepository;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RepositoryStd.Database.Repository
 {
     class EFOrderRepository : IOrderRepository
     {
+        private StockDbContext context;
+        public EFOrderRepository(StockDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Order> Orders => context.Orders
+        .Include(o => o.Lines)
+        .ThenInclude(l => l.Symbol);
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Symbol));
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
 
     }
 }
