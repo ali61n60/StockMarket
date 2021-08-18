a5a92711ee9adb23a599d5dab321dc9a6da6a463
Ali <ali62n62@yahoo.com>
2021-08-18 23:32:51 +0430

    27/5/1400

---
+++ StockMVC/Parameters/AliCountParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMVC.Parameters
+{
+    public class AliCountParameters
+    {
+        public int currentCount = 399;
+        
+        public void Decrement()
+        {
+            Console.WriteLine("Calling");
+            currentCount--;
+        }
+    }
+}
