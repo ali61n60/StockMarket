eb08d8ff371835e1fde11325611b46f07bb0c564
Ali <ali62n62@yahoo.com>
2021-09-29 15:56:05 +0330

    sendingrepeating data to signalr recievers

--- StockMVC/Services/PushNotificationService.cs
+++ StockMVC/Services/PushNotificationService.cs
@@ -4,19 +4,26 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
+using StockMVC.Hubs;
 
 namespace StockMVC.Services
 {
     public class PushNotificationService : IHostedService
     {
         private Timer _timer;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        
+        public PushNotificationService(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             // timer repeates call to RemoveScheduledAccounts every 24 hours.
+            
             _timer = new Timer(
                 SendMessage,
                 null,
@@ -34,10 +41,14 @@ namespace StockMVC.Services
             return Task.CompletedTask;
         }
 
-        public void SendMessage(Object state)
+        public async void SendMessage(Object state)
         {
-            //Console.WriteLine("called from repeating task at "+DateTime.Now.ToLongTimeString());
+            //get stock price and send it to users
+             await _hubContext.Clients.All.SendAsync("ReceiveMessage", "Server", "called from repeating task at " + DateTime.Now.ToLongTimeString());
+
             Debug.WriteLine("called from repeating task at " + DateTime.Now.ToLongTimeString());
+
+            
         }
 
         
