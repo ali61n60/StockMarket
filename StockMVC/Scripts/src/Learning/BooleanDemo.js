c10e3038994bd83b6540c7c2b257c8b88695ee1c
Ali <ali62n62@yahoo.com>
2021-10-07 22:32:46 +0330

    learning typescript

---
+++ StockMVC/Scripts/src/Learning/BooleanDemo.js
@@ -0,0 +1,11 @@
+function BooleanDemo(isOld) {
+    if (isOld) {
+        console.log("You Are Old");
+    }
+    else {
+        console.log("You Are Not Old");
+    }
+}
+BooleanDemo(true);
+BooleanDemo(false);
+//# sourceMappingURL=BooleanDemo.js.map
---
+++ StockMVC/Scripts/src/Learning/RestParams.ts
@@ -0,0 +1,15 @@
+﻿{
+    function AddWithRest(num1: number, num2: number, ...numbers: number[]): number {
+        let total = num1 + num2;
+        for (var i = 0; i < numbers.length; i++) {
+            total += numbers[i];
+        }
+        return total;
+    }
+
+    let answer = AddWithRest(14, 23, 30);
+    console.log("adding three numbers: " + answer);
+    answer = AddWithRest(14, 23);
+    console.log(`adding two numbers: ${answer}`);
+
+}
