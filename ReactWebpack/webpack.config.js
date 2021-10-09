ab3767c1dbcb633d9fb5b303fb88ac743b106038
anejati <anejati@115Q216Q>
2021-10-09 12:11:47 +0330

    webpack

---
+++ ReactWebpack/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require("path");
+module.exports = {
+    mode: "development",
+    entry: "./ wwwroot / js / app.js",
+    output: {
+        path: path.resolve(__dirname, "./ wwwroot / js / dist"),
+        filename: "bundle.js"
+    }
+};
