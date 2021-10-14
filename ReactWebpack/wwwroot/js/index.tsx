7757477c8970c8b56df2d8827a59d3c054bb5a37
Ali <ali62n62@yahoo.com>
2021-10-15 02:29:18 +0330

    this configuration works for babel typescript webpack

--- ReactWebpack/wwwroot/js/index.tsx
+++ ReactWebpack/wwwroot/js/index.tsx
@@ -1,30 +1,32 @@
 ﻿import  React from 'react';
 import  ReactDOM from 'react-dom';
 import  List from "./List"; 
-import  LikeButton from "./LikeButton";
+import LikeButton from "./LikeButton";
+
+const globalScope = (window /* browser */ || global /* node */) as any;
+function RunList(myList) {
+    ReactDOM.render(e(List), app1);
+}
 
 
-//function RunList(myList) {
-//    ReactDOM.render(e(List), app1);
-//}
-//window.RunList = RunList;
+export function MyAlert(name) {
+    alert("Calling MyAlert again" + name);
+}
 
-//function MyAlert(name) {
-//    alert("Calling MyAlert again" + name);
-//}
-//window.MyAlert = MyAlert; 
 
+globalScope.MyAlert = MyAlert;
+globalScope.RunList = RunList;
 
 const e = React.createElement;
 var app1 = document.getElementById("app1");
 //ReactDOM.render(e(List), app1);  
-ReactDOM.render(<List />, app1);   
+//ReactDOM.render(<List />, app1);   
 
 const domContainer = document.querySelector('#app2');
 ReactDOM.render(<LikeButton />, domContainer);
 
 var app3 = document.getElementById('app3');   
-ReactDOM.render(<div> <p>Hello, world! 123</p> </div>, app3);  
+ReactDOM.render(<div> <p>Hello, world! 123</p> </div>, app3);   
 
 
 
