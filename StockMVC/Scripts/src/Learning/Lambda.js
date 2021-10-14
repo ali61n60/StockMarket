1b52d3239ba6fff7cf9dcc4217f67a44d2a8502d
Ali <ali62n62@yahoo.com>
2021-10-14 15:26:59 +0330

    ok

---
+++ StockMVC/Scripts/src/Learning/Lambda.js
@@ -0,0 +1,12 @@
+var MultiplyIt = function (num1, num2) {
+    return num1 * num2;
+};
+var MultiplyItLambda = function (num1, num2) { return num1 * num2; };
+var SayFirstNumber;
+SayFirstNumber = function (first) {
+    console.log(first);
+};
+console.log(MultiplyIt(7, 8));
+console.log(MultiplyItLambda(7, 8));
+SayFirstNumber(897);
+//# sourceMappingURL=Lambda.js.map
---
+++ StockMVC/Scripts/src/Learning/NumberDemo.js
@@ -0,0 +1,5 @@
+function NumberDemo(age, lengthOfEmployment) {
+    console.log("You are " + age + " old, and you have worked here " + lengthOfEmployment + " years");
+}
+NumberDemo(37, 10);
+//# sourceMappingURL=NumberDemo.js.map
