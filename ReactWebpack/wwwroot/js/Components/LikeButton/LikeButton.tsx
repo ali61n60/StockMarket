891d0bdb686631e6ffce1640e233a6985d0b6bec
Ali <ali62n62@yahoo.com>
2021-10-15 18:12:37 +0330

    typescript working again

---
+++ ReactWebpack/wwwroot/js/Components/LikeButton/LikeButton.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from "react";
+
+interface MyProps { }
+
+interface MyState {
+    liked: boolean;
+}
+
+export default class LikeButton extends React.Component<MyProps,MyState > {
+    constructor(props:MyProps) {
+        super(props);
+        this.state = { liked :false };
+    }
+    
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return React.createElement(
+            'button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+    } 
+}
+
--- ReactWebpack/wwwroot/js/index.tsx
+++ ReactWebpack/wwwroot/js/index.tsx
@@ -1,23 +1,26 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as  List from "./List";
-import Button from "./Components/Button/index"
-
-
-
-
+import List from "./Components/List/List";
+import LikeButton from "./Components/LikeButton/LikeButton";
+import Button from "./Components/Button/Button" 
 
 
 
 const e = React.createElement;
 
+
 var app1 = document.getElementById("app1");
-ReactDOM.render(<Button />, app1);   
+ReactDOM.render(<List />, app1);
+
+var app2 = document.getElementById("app2");
+ReactDOM.render(<LikeButton />, app2);
 
 
 var app3 = document.getElementById("app3");   
 ReactDOM.render(<div> <p>Hello, world! 1234</p> </div>, app3); 
 
  
+var app4 = document.getElementById("app4");
+ReactDOM.render(<Button />, app4);
 
 
