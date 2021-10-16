0145dee703460e7c8f696fb241321805c60cdf4e
anejati <anejati@115Q216Q>
2021-10-16 15:25:55 +0330

    handling click event of a react button

--- ReactWebpack/wwwroot/js/Components/Button/Button.tsx
+++ ReactWebpack/wwwroot/js/Components/Button/Button.tsx
@@ -4,21 +4,32 @@
 interface Props {
     type:"primary"|"default";
 }
+interface State {
+    changed: boolean;
+}
 
-export default class Button extends React.Component<Props,{}> {
+export default class Button extends React.Component<Props, State> {
+    i: number=19;
     constructor(props: Props) {
         super(props);
+        this.state = { changed: false };
         
     }
+    
 
     render() {
-        const className = this.props.type === "primary" ? "btn-primary" : "btn-danger";
+        const className = this.props.type === "primary" ? "btn btn-block btn-primary" : "btn btn-block btn-danger";
         return (                
             <div>
-                <button className={className} >Button</button>
-                <h1>bu</h1>
+                <p className="text-center">number is: {this.i}</p>
+                <button className={className} onClick={this.handleClick}  >Button</button>                
             </div> 
         );
+    }
+
+    handleClick = () => {
+        this.i++;
+        this.setState({ changed: true });        
     }
 }
 
