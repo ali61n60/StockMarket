3c63133994d41d119ab83cd0c3ee87f8ca35fc6f
anejati <anejati@115Q216Q>
2021-10-19 16:05:48 +0330

    copy react js files to stockmvc project

---
+++ StockMVC/webpack.config.js
@@ -0,0 +1,36 @@
+﻿const path = require("path");
+
+
+module.exports = {   
+    mode:"development",
+    entry: "./wwwroot/js/index.tsx",
+    devtool: "inline-source-map",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./wwwroot/dist"),
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: "css-loader" },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react"]
+                    }
+                    
+                }
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js","jsx", ".ts", ".tsx" ],
+    }
+};
---
+++ StockMVC/wwwroot/js/Components/LikeButton/LikeButton.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from "react";
+
+interface Props { }
+
+interface State {
+    liked: boolean;
+}
+
+export default class LikeButton extends React.Component<Props,State > {
+    constructor(props:Props) {
+        super(props);
+        this.state = { liked :false };
+    }
+    
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return React.createElement(
+            'button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+    } 
+}
+
