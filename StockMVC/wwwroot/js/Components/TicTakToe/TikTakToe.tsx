1c2ec872ee1556846c0700e12308faa77fd2f719
Ali <ali62n62@yahoo.com>
2021-10-25 00:45:25 +0330

    ok

--- StockMVC/wwwroot/js/Components/TicTakToe/TikTakToe.tsx
+++ StockMVC/wwwroot/js/Components/TicTakToe/TikTakToe.tsx
@@ -1,11 +1,38 @@
 ﻿import * as React from "react";
 
-export default class Game extends React.Component {
+
+interface IGameProps {
+    
+}
+
+interface IGameStates {
+    history: [{squares: Array<string>}];
+    xIsNext:boolean;
+}
+export default class Game extends React.Component<IGameProps,IGameStates> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current.squares} xIsNext={true} onClick={() => {}} />
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -17,54 +44,61 @@ export default class Game extends React.Component {
 }
 
 interface ISquarProp {
-    value:number;
+    value: string;
+    onClick;
 }
-
 interface  ISquareState {
     value:string;
 }
-
 class Square extends React.Component<ISquarProp, ISquareState> {
-    constructor(props:ISquarProp) {
-        super(props);
-        this.state = { value: "" };
-    }
-
+    
     render() {
         return (
             <button className="square" onClick={this.buttonClick}>
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 
     buttonClick=() => {
-         this.setState({value:"X"});
+        this.props.onClick();
     }
 }
 
-
 interface IBoardProp {
-    
+     squares: Array<string>;
+    xIsNext: boolean;
+    onClick;
 }
 
 interface IBoardState {
     squares: Array<string>;
+    xIsNext:boolean;
 }
 class Board extends React.Component<IBoardProp,IBoardState> {
-    constructor(props: IBoardProp) {
-        super(props);
-        this.state = { squares: Array(9).fill(null) };
-       
+    
+    handleClick = (i: number) => {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O";
+    }
+    renderSquare(i:number) {
+        return <Square value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)} />;
     }
 
-    renderSquare(i) {
-        return <Square value={i} />;
-    }
+   
 
     render() {
-        const status = 'Next player: X';
-
+        const winner = calculateWinner(this.props.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -90,4 +124,22 @@ class Board extends React.Component<IBoardProp,IBoardState> {
 
 
 // ========================================
-
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
