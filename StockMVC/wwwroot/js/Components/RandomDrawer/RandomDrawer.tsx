c989d71dce19db27ba448d7b3cb6bdf2672e3bd9
Ali <ali62n62@yahoo.com>
2021-11-19 21:20:44 +0330

    28/8/1400

--- StockMVC/wwwroot/js/Components/RandomDrawer/RandomDrawer.tsx
+++ StockMVC/wwwroot/js/Components/RandomDrawer/RandomDrawer.tsx
@@ -5,30 +5,30 @@ class Point {
     y: number;
 }
 
-interface Props { 
-    
+interface Props {
+
 }
 interface State {
-    
+
 }
 
 export default class RandomDrawer extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-       
+
     }
     canvasStyle = {
-        width: "200px",
-        height: "200px",
-        border:"1px solid blue"
+        border: "1px solid blue"
     } as const;
     drawerId: any;
-    chartRef = React.createRef() as any;
+    canvasRef = React.createRef() as any;
+    width = 200;
+    height = 200;
     p1: Point;
     p2: Point;
     canvasW: any;
-    canvasH: any ;
-   
+    canvasH: any;
+
     tick = () => {
         //create rondom point and draw it        
         this.p1.x = this.p2.x;
@@ -36,7 +36,6 @@ export default class RandomDrawer extends React.Component<Props, State> {
         this.p2.x = Math.floor(Math.random() * this.canvasW);
         this.p2.y = Math.floor(Math.random() * this.canvasH);
         this.DrawLine(this.p1, this.p2, "#" + Math.floor(Math.random() * 16777215).toString());
-    
     }
 
     componentDidMount() {
@@ -44,10 +43,10 @@ export default class RandomDrawer extends React.Component<Props, State> {
         this.p2 = new Point();
         this.p2.x = 0;
         this.p2.y = 0;
-        this.canvasW = this.chartRef.current.getBoundingClientRect().width;
-        this.canvasH = this.chartRef.current.getBoundingClientRect().height;
+        this.canvasW = this.canvasRef.current.getBoundingClientRect().width;
+        this.canvasH = this.canvasRef.current.getBoundingClientRect().height;
         console.log("W=" + this.canvasW + ", H=" + this.canvasH);
-        this.drawerId = setInterval(() => this.tick(), 1000); 
+        this.drawerId = setInterval(() => this.tick(), 100);
     }
 
     componentWillUnmount() {
@@ -56,19 +55,18 @@ export default class RandomDrawer extends React.Component<Props, State> {
 
     render() {
         return (
-            <canvas id="chart1" ref={this.chartRef} style={this.canvasStyle} />
-        );        
+            <canvas id="chart1" width={this.width} height={this.height} ref={this.canvasRef} style={this.canvasStyle} />
+        );
     }
 
     DrawLine(p1: Point, p2: Point, color: string): void {
-        const ctx = this.chartRef.current.getContext('2d');
-            // set line stroke and line width
-            ctx.strokeStyle = color;
-            ctx.lineWidth = 1;
-            // draw a red line
-            ctx.beginPath();
-            ctx.moveTo(p1.x, p1.y);
-            ctx.lineTo(p2.x, p2.y);
-            ctx.stroke();
+        const ctx = this.canvasRef.current.getContext("2d");
+        // set line stroke and line width
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.stroke();
     }
 }
