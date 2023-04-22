6ed28fc0d2e4aa4a737ca693465c8448560b6592
Ali <ali62n62@yahoo.com>
2023-04-23 02:27:02 +0330

    small changes

--- StockMarket/FormLive.cs
+++ StockMarket/FormLive.cs
@@ -10,14 +10,11 @@ namespace StockMarket
         private readonly string ParsUrl = "http://www.tsetmc.ir/tsev2/data/instinfodata.aspx?i=6110133418282108&c=44+";
         private readonly string GhadirUrl = "http://www.tsetmc.ir/tsev2/data/instinfofast.aspx?i=26014913469567886&c=39+";
         private readonly string ZagrosUrl = "http://www.tsetmc.ir/tsev2/data/instinfodata.aspx?i=13235547361447092&c=44+";
-        private readonly string Zasta2018 = "http://cdn.tsetmc.com/api/BestLimits/17494496342776868";
+        private readonly string Zasta2018BestLimits = "http://cdn.tsetmc.com/api/BestLimits/17494496342776868";
+        private readonly string Zasta2018Closing = "http://cdn.tsetmc.com/api/ClosingPrice/GetClosingPriceInfo/1749449634277686";
         private int numberOfCalls = 0;
 
-        private readonly TseLiveData tseLiveData;
-
-        bool runRatio = false;
-
-
+        
         public FormLive()
         {
             InitializeComponent();
@@ -34,7 +31,7 @@ namespace StockMarket
             userControlLiveData1.StrikePrice = "13";
             userControlLiveData1.DaysToApply = "14";
             userControlLiveData1.ProfitInPercent = "15";
-            userControlLiveData1.Url = Zasta2018;
+            userControlLiveData1.Url = Zasta2018BestLimits;
             userControlLiveData1.UrlBase = "";
             userControlLiveData1.liveDataWorker = new OptionLiveData(userControlLiveData1.Url);
             userControlLiveData1.UpdateData();
