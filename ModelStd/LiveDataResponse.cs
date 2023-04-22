b68fc85965070a4896ff024fdcda6031eee87a61
Ali <ali62n62@yahoo.com>
2023-04-22 13:14:55 +0330

    working on live data Task async

---
+++ ModelStd/LiveDataResponse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelStd
+{
+    public class LiveDataResponse
+    {
+        public double Price;
+        public bool IsResultOk;
+        public string Message;
+    }
+
+    public class SymbolData
+    {
+        public string Time;
+        public string A;
+        public string TransactionPrice;
+        public string Final;
+        public string Sell;
+        public string YesterdayFinal;
+        public string MinDayPrice;
+        public string MaxDayPrice;
+        public string Counter;
+        public string Volume;
+        public string TransactionValue;
+        public string Zero;
+        public string DateEnglish;
+        public string Time2;
+        public string DateTime;
+        public string State;
+        public string NotDiscovered1;
+        public string NotDiscovered2;
+        public string NotDiscovered3;
+        public string NotDiscovered4;
+        public string NotDiscovered5;
+        public string NotDiscovered6;
+        public string NotDiscovered7;
+        public string NotDiscovered8;
+        public string NotDiscovered9;
+        public string NotDiscovered10;
+        public string NotDiscovered11;
+        public string NotDiscovered12;
+        public string NotDiscovered13;
+        public string NotDiscovered14;
+        public string OrderRow1;
+        public string OrderRow2;
+        public string OrderRow3;
+        public string NotDiscovered15;
+        public string NotDiscovered16;
+        public string NotDiscovered17;
+    }
+}
---
+++ ServiceStd/LiveData/ILiveData.cs
@@ -0,0 +1,13 @@
+﻿using ModelStd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStd.LiveData
+{
+    interface ILiveData
+    {
+        Task<LiveDataResponse> GetPriceAsync();
+    }
+}
