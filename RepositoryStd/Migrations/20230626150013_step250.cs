9612a42fc835524f324d608fff4af0da115b93f8
anejati <anejati@115Q216Q>
2023-06-26 19:41:03 +0330

    ok

---
+++ RepositoryStd/Migrations/20230626150013_step250.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryStd.Migrations
+{
+    public partial class step250 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
